283cf6d08b89058d746a96a7f9d008add1ef7aec
Team-SB-288 <teamstudybud@gamil.com>
2024-10-14 19:11:19 +0530

    added link to landing page button

---
+++ index.js
@@ -0,0 +1,7 @@
+function RedirectToLogin() {
+    window.location.href = "login.html"; // Change URL to the desired page
+}
+
+function RedirectToSignUp() {
+    window.location.href = "signup.html"; // Change URL to the desired page
+}
