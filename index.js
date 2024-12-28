22900067dd61a918c67d3591c144401a5bf18509
sagar-8426 <sagarnarigara69@gmail.com>
2024-12-28 12:35:34 +0530

    hi hi

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-let login = document.getElementById('login-btn');
-let register = document.getElementById('register-btn');
-
-login.addEventListener('click', () => {
-  window.location.href = 'login.html';
-})
-register.addEventListener('click', () => {
-  window.location.href = 'register.html';
-})
-=======
 function loadPage(url) {
   window.location.href = url; // Redirects to the specified URL
 }
@@ -21,4 +10,3 @@ document.getElementById('lgbtn').addEventListener('click', function() {
 document.getElementById('rgbtn').addEventListener('click', function() {
   loadPage('register.html'); // Redirect to the register page
 });
->>>>>>> a20b91df91cf93a53c68079aad8cd8787435106e
