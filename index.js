a20b91df91cf93a53c68079aad8cd8787435106e
Jay09990 <jdudhrejiya0990@gmail.com>
2024-12-28 12:16:17 +0530

    added eventlisteenr to index

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
-function RedirectToLogin() {
-    window.location.href = "login.html"; // Change URL to the desired page
+function loadPage(url) {
+  window.location.href = url; // Redirects to the specified URL
 }
 
-function RedirectToSignUp() {
-    window.location.href = "register.html"; // Change URL to the desired page
-}
+// Attach event listeners to buttons
+document.getElementById('lgbtn').addEventListener('click', function() {
+  loadPage('login.html'); // Redirect to the login page
+});
+
+document.getElementById('rgbtn').addEventListener('click', function() {
+  loadPage('register.html'); // Redirect to the register page
+});
